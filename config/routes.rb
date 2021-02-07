d66290b1a0c253bcea29f129aff357da3f3bc0a8
OJ De Quintos <odequintos@yondu.com>
2021-02-07 16:24:50 +0800

    Viewing Recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      get 'recipes/create'
+      get 'recipes/show'
+      get 'recipes/destroy'
+    end
+  end
   root 'homepage#index'
 end
