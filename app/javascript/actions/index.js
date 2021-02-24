9433413009d545c6e56ee44aadc38e5318f3ad8f
OJ De Quintos <odequintos@yondu.com>
2021-02-24 15:14:08 +0800

    Fix action

--- app/javascript/actions/index.js
+++ app/javascript/actions/index.js
@@ -1,5 +1,22 @@
+const GET_RECIPES_REQUEST = 'GET_RECIPES_REQUEST';
+const GET_RECIPES_SUCCESS = 'GET_RECIPES_SUCCESS';
+
 export const ALL_RECIPES = () => {
+  console.log('ALL_RECIPES() Action!!')
+  return dispatch => {
+    dispatch({ type: GET_RECIPES_REQUEST });
+    return fetch(`/api/v1/recipes/index`)
+    .then(response => response.json())
+    .then(json => dispatch(getRecipesSuccess(json)))
+    .catch(error => console.log(error));
+  }
+}
+
+export function getRecipesSuccess(json) {
+  console.log('getRecipesSuccess() Action!!')
+  console.log(json)
   return {
-    type: 'ALL_RECIPES'
+    type: GET_RECIPES_SUCCESS,
+    json
   }
 }
--- app/javascript/components/Recipe/index.js
+++ app/javascript/components/Recipe/index.js
@@ -1,29 +1,7 @@
 import React from "react"
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-
-const GET_RECIPES_REQUEST = 'GET_RECIPES_REQUEST';
-const GET_RECIPES_SUCCESS = 'GET_RECIPES_SUCCESS';
-
-function ALL_RECIPES() {
-  console.log('ALL_RECIPES() Action!!')
-  return dispatch => {
-    dispatch({ type: GET_RECIPES_REQUEST });
-    return fetch(`/api/v1/recipes/index`)
-    .then(response => response.json())
-    .then(json => dispatch(getRecipesSuccess(json)))
-    .catch(error => console.log(error));
-  }
-}
-
-export function getRecipesSuccess(json) {
-  console.log('getRecipesSuccess() Action!!')
-  console.log(json)
-  return {
-    type: GET_RECIPES_SUCCESS,
-    json
-  }
-}
+import { ALL_RECIPES } from '../../actions';
 
 class Recipe extends React.Component {
   render () {
